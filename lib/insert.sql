dd675ff56163814097e11e17b6df7da9affde48f
Yeji-Yoon <angelayoon0102@gmail.com>
2025-03-13 19:25:30 -0700

    s

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'blue','good', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 1, 'M','blue','good', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F','blue','good', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M','blue','good', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'M','blue','good', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M','blue','good', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F','blue','good', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, 'F','blue','good', True);
+
+
+
